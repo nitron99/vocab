a31f5f0715fcd65e74e7a2df64a75ca12edb6806
Nitin Kumar Singh <nitin99bop2cod2f@gmail.com>
2023-08-20 14:55:46 +0530

    linting v2

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -42,10 +42,10 @@ export function AuthProvider({children}) {
         const errorCode = error.code;
         const errorMessage = error.message;
         // The email of the user's account used.
-        const email = error.customData.email;
+        // const email = error.customData.email;
         // The AuthCredential type that was used.
         console.log(errorCode, errorMessage)
-        const credential = GoogleAuthProvider.credentialFromError(error);
+        // const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
   }
